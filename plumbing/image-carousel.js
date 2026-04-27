734794cb1531ef8a69e1ab2a3e62c542cb63801f
PhilemonVanDussen <vandussenph709@northwested.org>
2026-04-27 14:30:11 -0400

    update 9

---
+++ plumbing/image-carousel.js
@@ -0,0 +1,14 @@
+let slideIndex = 1
+showSlides(slideIndex)
+
+function plusSlides(n) {
+    showSlides(slideIndex = n);
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n); 
+}
+
+function showSlides() {
+
+}
